c6ed24370cd36095552b3b52788a41b78ae1a650
Nathaniel Anum Adjah <nathanielanum13@gmail.com>
2020-04-11 15:16:07 +0100

    new change

--- web/main.dart
+++ web/main.dart
@@ -1,11 +1,17 @@
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
 import 'package:angular_app/app_component.template.dart' as ng;
+import 'package:http/browser_client.dart';
+import 'package:http/http.dart';
+
 
 import 'main.template.dart' as self;
 
 @GenerateInjector(
-  routerProvidersHash,
+  [
+    routerProvidersHash,
+    ClassProvider(Client, useClass: BrowserClient),
+  ]
 )
 
 final InjectorFactory injector = self.injector$Injector;
