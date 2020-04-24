6d4ecc4cec0460852d333c5874f4bc07ad8b110b
PRINCE BOBBY <pbobby001@st.ug.edu.gh>
2020-04-24 11:32:34 +0100

    Implement responsive material layout

--- lib/src/dashboard_component/dashboard_component.dart
+++ lib/src/dashboard_component/dashboard_component.dart
@@ -1,10 +1,27 @@
 import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
 
 @Component(
   selector: 'dashboard-app',
   templateUrl: 'dashboard_component.html',
-  directives: [coreDirectives], 
+  styleUrls: [
+    'dashboard_component.css',
+    'package:angular_components/app_layout/layout.scss.css',
+  ],
+  directives: [
+    coreDirectives,
+    DeferredContentDirective, 
+    MaterialPersistentDrawerDirective,
+    MaterialTemporaryDrawerComponent,
+    MaterialButtonComponent,
+    MaterialIconComponent,
+    MaterialListComponent,
+    MaterialListItemComponent,
+    MaterialToggleComponent,
+  ], 
 )
 class DashboardComponent{
-
+  bool customWidth = false;
+  bool end = false;
+  bool overlay = false;
 }
