1dba6fa02607ca658e1c67b9c8b7215381324878
PRINCE BOBBY <pbobby001@st.ug.edu.gh>
2020-05-07 12:47:31 +0100

    Implement pwa

--- web/main.dart
+++ web/main.dart
@@ -3,6 +3,7 @@ import 'package:angular_router/angular_router.dart';
 import 'package:angular_app/app_component.template.dart' as ng;
 import 'package:http/browser_client.dart';
 import 'package:http/http.dart';
+import 'package:pwa/client.dart' as pwa;
 
 
 import 'main.template.dart' as self;
@@ -17,5 +18,7 @@ import 'main.template.dart' as self;
 final InjectorFactory injector = self.injector$Injector;
 
 void main() {
+  pwa.Client();
+
   runApp(ng.AppComponentNgFactory, createInjector: injector);
 }
