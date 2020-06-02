31d6b4c58977d82f7a8794b5aee66558882af714
PRINCE BOBBY <pbobby001@st.ug.edu.gh>
2020-06-02 11:56:17 +0100

    Implement the assignment component

--- lib/src/dashboards/student_dashboard_component/assignment_component/assignment_component.dart
+++ lib/src/dashboards/student_dashboard_component/assignment_component/assignment_component.dart
@@ -1,10 +1,20 @@
 import 'package:angular/angular.dart';
+import 'package:angular_app/src/dashboards/student_dashboard_component/assignment_component/service/assignment_service.dart';
 
 @Component(
   selector: 'assignment-component',
   templateUrl: 'assignment_component.html',
+  providers: [ClassProvider(AssignmentService)],
 )
 
-class AssignmentComponent {
+class AssignmentComponent implements OnInit {
+
+  final AssignmentService _assignmentService;
+  AssignmentComponent(this._assignmentService);
+
+  @override
+  void ngOnInit(){
+    print("Hello");
+  }
 
 }
