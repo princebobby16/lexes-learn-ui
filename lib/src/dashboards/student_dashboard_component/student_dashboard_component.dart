7589bcefd021e931150be23c7087f8bcac358323
PRINCE BOBBY <pbobby001@st.ug.edu.gh>
2020-06-04 16:27:33 +0000

    Revert "Implement the assignment component"
    This reverts commit 31d6b4c58977d82f7a8794b5aee66558882af714

--- lib/src/dashboards/student_dashboard_component/student_dashboard_component.dart
+++ lib/src/dashboards/student_dashboard_component/student_dashboard_component.dart
@@ -1,14 +1,10 @@
-import 'dart:html';
-
 import 'package:angular/angular.dart';
-import 'package:angular_app/src/dashboards/student_dashboard_component/home_component/home_component.dart';
+//import 'package:angular_app/src/dashboards/student_dashboard_component/chat_component/chat_component.dart';
 import 'package:angular_app/src/dashboards/student_dashboard_component/routes.dart';
 import 'package:angular_components/angular_components.dart';
 import 'package:angular_router/angular_router.dart';
 
 import 'dart:async';
-
-import 'package:stagexl/stagexl.dart';
 
 @Component(
   selector: 'student_dashboard-app',
@@ -27,7 +23,7 @@ import 'package:stagexl/stagexl.dart';
     MaterialListComponent,
     MaterialListItemComponent,
     MaterialToggleComponent,
-    HomeComponent,
+//    ChatComponent,
     routerDirectives
   ], 
   exports: [RoutePaths, Routes]
@@ -43,18 +39,15 @@ class StudentDashboardComponent {
   Future<void> gotoChat() async {
     print("Chat");
     _router.navigate(RoutePaths.chat.toUrl());
-  }
-  
+  } 
   Future<void> goHome() async  {
     print("Home");
     _router.navigate(RoutePaths.home.toUrl());
   }
-  
   Future<void> gotoAssignment() async {
     print("assignment");
-     _router.navigate(RoutePaths.assignments.toUrl());
-  }
-  
+    // _router.navigate(RoutePaths.assignments.toUrl());
+  } 
   Future<void> gotoJoinClass() async  {
     print("Class");
     _router.navigate(RoutePaths.join_class.toUrl());
--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -1,37 +1,40 @@
 import 'package:angular_router/angular_router.dart';
-//import 'home_component/home_component.template.dart' as home_template;
+import 'home_component/home_component.template.dart' as home_template;
 import 'signin_component/signin_component.template.dart' as signin_template;
-import 'dashboards/student_dashboard_component/student_dashboard_component.template.dart'
-    as student_dashboard_template;
-import 'dashboards/teacher_dashboard_component/teacher_dashboard_component.template.dart'
-    as teacher_dashboard_template;
+import 'dashboards/student_dashboard_component/student_dashboard_component.template.dart' as student_dashboard_template;
+import 'dashboards/teacher_dashboard_component/teacher_dashboard_component.template.dart' as teacher_dashboard_template;
 
 import 'route_path.dart';
 export 'routes.dart';
 
-class Routes {
-//  static final home = RouteDefinition(
-//    routePath: RoutePaths.home,
-//    component: home_template.HomeComponentNgFactory,
-//  );
+class Routes{
+  static final home = RouteDefinition(
+    routePath: RoutePaths.home,
+  component: home_template.HomeComponentNgFactory,
+  useAsDefault: true,
+
+  );
   static final signin = RouteDefinition(
     routePath: RoutePaths.signin,
     component: signin_template.SigninComponentNgFactory,
-    useAsDefault: true,
   );
   static final student_dashboard = RouteDefinition(
     routePath: RoutePaths.student_dashboard,
     component: student_dashboard_template.StudentDashboardComponentNgFactory,
   );
+
+  // Dont forget to edit the component
   static final teacher_dashboard = RouteDefinition(
     routePath: RoutePaths.teacher_dashboard,
     component: teacher_dashboard_template.TeacherDashboardComponentNgFactory,
   );
-
-  static final all = <RouteDefinition>[
-//    home,
+  
+  //edited
+  static final all = <RouteDefinition> [  
+    home,
     signin,
     student_dashboard,
     teacher_dashboard,
   ];
+
 }
